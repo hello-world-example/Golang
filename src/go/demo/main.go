31fd285d793ed00b5e16053bfdc077baef0e8796
kaibin.yang <ykb553@163.com>
2020-09-29 16:43:07 +0800

    test

---
+++ src/go/demo/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+var Usage = func() {
+	fmt.Println("")
+	fmt.Println("split")
+	fmt.Println("	切割")
+	fmt.Println()
+}
+
+/*
+https://unidoc.io/examples/getting_started/
+ */
+func main() {
+	Usage()
+}
